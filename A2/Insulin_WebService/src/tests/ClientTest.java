343e2577c16910d86c2bd7b13ff560214fe6482d
jpleitao <jocaleitao93@gmail.com>
2015-06-13 12:31:32 +0100

    Multiple test cases with different parameters

--- A2/Insulin_WebService/src/tests/ClientTest.java
+++ A2/Insulin_WebService/src/tests/ClientTest.java
@@ -2,23 +2,40 @@ package tests;
 
 import client.Client;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import static org.junit.Assert.*;
 
+@RunWith(Parameterized.class)
 public class ClientTest {
 
     private Client client;
+    private int inputValue;
+    private int outputValue;
 
-    public ClientTest() {
+    public ClientTest(int input, int output) {
         client = new Client();
+        inputValue = input;
+        outputValue = output;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        Object[][] data = new Object[][] {{79, 22}};
+        return Arrays.asList(data);
     }
 
     @Test
     public void testBackgroundInsulinDose() throws Exception {
-        assertEquals(22, client.backgroundInsulinDose(79));
+        assertEquals(outputValue, client.backgroundInsulinDose(inputValue));
     }
 
+    /*
     @Test
     public void testMealtimeInsulinDose() throws Exception {
         assertEquals(14, client.mealtimeInsulinDose(60, 12, 200, 100, 25));
@@ -32,6 +49,7 @@ public class ClientTest {
         int[] banana2 = {50, 50};
         assertEquals(50, client.personalSensitivityToInsulin(5, arrayToList(banana), arrayToList(banana2)));
     }
+    */
 
     /**
      * Instead of passing an int[] to a web service, pass a List<Integer>.
