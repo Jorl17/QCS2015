630dbf072ac8339d8166aa2de7385ac5f4d41882
jpleitao <jocaleitao93@gmail.com>
2015-06-23 09:14:00 +0100

    Added test classes for the web wervice

--- A2/N-Version-Programming-master/WebServices/src/main/java/client/Client.java
+++ A2/N-Version-Programming-master/WebServices/src/main/java/client/Client.java
@@ -8,6 +8,57 @@ import java.util.Scanner;
 
 
 public class Client {
+
+    ////
+    // Begin of the developed code to test the Insulin Dose Calculator Web Service
+    ////
+
+    private InsulinDoseCalculator proxy;
+    private InsulinDoseCalculatorService service;
+
+    public Client() {
+        service = new InsulinDoseCalculatorService();
+        proxy = service.getInsulinDoseCalculatorPort();
+    }
+
+    public int backgroundInsulinDose(int value)
+    {
+        return proxy.backgroundInsulinDose(value);
+    }
+
+    public int mealtimeInsulinDose(int arg0, int arg1, int arg2, int arg3, int arg4)
+    {
+        return proxy.mealtimeInsulinDose(arg0, arg1, arg2, arg3, arg4);
+    }
+
+    public int personalSensitivityToInsulin(int arg0, List<Integer> list0, List<Integer> list1)
+    {
+        return proxy.personalSensitivityToInsulin(arg0, list0, list1);
+    }
+
+    /**
+     * Instead of passing an int[] to a web service, pass a List<Integer>.
+     * <p>
+     * This method converts an array of int to a List of Integer. When a web
+     * service publishes a method that receives an int[] as parameter, the type
+     * gets converted to a "sequence" in XML, and the client should therefore
+     * submit, as parameter, a List<Integer> instead of an int[]. The web
+     * service will still receive an int[] on the server side.
+     *
+     * @param array the array of int, to be converted to List<Integer>
+     * @return a list containing the same numbers as the array
+     */
+    static List<Integer> arrayToList(int[] array) {
+        List<Integer> list = new ArrayList<Integer>();
+        for(int i : array)
+            list.add(i);
+        return list;
+    }
+
+    ////
+    // End of the developed code to test the Insulin Dose Calculator Web Service
+    ////
+
     public static void main(String[] argv)
     {
         //Fazer isto depois para múltiplos web services
---
+++ A2/N-Version-Programming-master/WebServices/src/main/java/tests/PersonalSensitivityToInsulinTest.java
@@ -0,0 +1,63 @@
+package tests;
+
+import client.Client;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import static org.junit.Assert.*;
+
+@RunWith(Parameterized.class)
+public class PersonalSensitivityToInsulinTest {
+
+    private Client client;
+    private int inputValue;
+    private int[] inputArray0;
+    private int[] inputArray1;
+    private int outputValue;
+
+    public PersonalSensitivityToInsulinTest(int arg0, int[] list0, int[] list1, int output) {
+        client = new Client();
+        inputValue = arg0;
+        inputArray0 = list0;
+        inputArray1 = list1;
+        outputValue = output;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        //Generate the pairs of input and output tests
+        int[] banana = {0, 10};
+        int[] banana2 = {50, 50};
+
+        Object[][] data = new Object[][] {{5, banana, banana2, 50}};
+        return Arrays.asList(data);
+    }
+
+    @Test
+    public void testPersonalSensitivityToInsulin() throws Exception {
+        assertEquals(outputValue, client.personalSensitivityToInsulin(inputValue, arrayToList(inputArray0), arrayToList(inputArray1)));
+    }
+
+    /**
+     * Instead of passing an int[] to a web service, pass a List<Integer>.
+     * <p>
+     * This method converts an array of int to a List of Integer. When a web
+     * service publishes a method that receives an int[] as parameter, the type
+     * gets converted to a "sequence" in XML, and the client should therefore
+     * submit, as parameter, a List<Integer> instead of an int[]. The web
+     * service will still receive an int[] on the server side.
+     *
+     * @param array the array of int, to be converted to List<Integer>
+     * @return a list containing the same numbers as the array
+     */
+    public List<Integer> arrayToList(int[] array) {
+        List<Integer> list = new ArrayList<>();
+        for (int i : array)
+            list.add(i);
+        return list;
+    }
+}
